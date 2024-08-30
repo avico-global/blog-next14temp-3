c5faa5481e64daea379906802c093a80347498ec
Faaiz046 <119515903+Faaiz046@users.noreply.github.com>
2024-08-30 19:04:08 +0500

    revert code

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,6 @@ import BlogCard from "@/components/common/BlogCard";
 
 import {
   callBackendApi,
-  downloadImages,
   getDomain,
   getImagePath,
   robotsTxt,
@@ -41,9 +40,7 @@ export default function Home({
   layout,
   tag_list,
   nav_type,
-  testData
 }) {
-  console.log("👊 ~ testData:", testData)
   const page = layout?.find((page) => page.page === "home");
 
   return (
@@ -361,8 +358,8 @@ export async function getServerSideProps({ req }) {
     type: "contact_details",
   });
   let project_id = logo?.data[0]?.project_id || null;
-  const testData=await downloadImages({domain, project_id});
-  console.log("👊 ~ getServerSideProps ~ testData:", testData)
+  // const testData=await downloadImages({domain, project_id});
+  // console.log("👊 ~ getServerSideProps ~ testData:", testData)
   const about_me = await callBackendApi({ domain, type: "about_me" });
   const copyright = await callBackendApi({ domain, type: "copyright" });
   const banner = await callBackendApi({ domain, type: "banner" });
@@ -376,7 +373,6 @@ export async function getServerSideProps({ req }) {
 
   return {
     props: {
-      testData,
       domain,
       imagePath,
       meta: meta?.data[0]?.value || null,
