bb8c41a4db14c6e83969c6c0fcf97e68d31971a9
Usama Bhatti <70816277+usamamustafa778@users.noreply.github.com>
2024-09-01 11:11:42 +0500

    apos error fixed

--- pages/404.jsx
+++ pages/404.jsx
@@ -8,11 +8,12 @@ const Custom404 = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <div className="text-9xl font-bold text-red-500 mb-4">404</div>
       <h1 className="text-3xl font-semibold text-gray-800 mb-4">
-        Oops! The page you are looking for doesn't exist.
+        {"Oops! The page you are looking for doesn't exist."}
       </h1>
       <p className="text-lg text-gray-600 mb-8">
-        It seems you've hit a broken link or entered a URL that doesn't exist on
-        this site.
+        {
+          "It seems you've hit a broken link or entered a URL that doesn't exist on this site."
+        }
       </p>
       <Link
         href="/"
