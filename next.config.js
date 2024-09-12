51f38d1ed6ff0ecfe3a9d28095118adde38b1ed9
Usama Bhatti <70816277+usamamustafa778@users.noreply.github.com>
2024-09-12 13:37:01 +0500

    next config

--- next.config.js
+++ next.config.js
@@ -8,10 +8,18 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "www.custom-wheels-car-rims.com",
+      },
+      {
+        protocol: "https",
         hostname: "custom-wheels-car-rims.com",
       },
     ],
-    domains: ["api.sitebuilderz.com", "custom-wheels-car-rims.com"],
+    domains: [
+      "api.sitebuilderz.com",
+      "www.custom-wheels-car-rims.com",
+      "custom-wheels-car-rims.com",
+    ],
   },
   reactStrictMode: true,
 };
