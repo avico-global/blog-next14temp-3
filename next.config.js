7efdb165f06449229e01496058b2e2e99bfb5fc2
Usama Bhatti <70816277+usamamustafa778@users.noreply.github.com>
2024-09-12 13:16:36 +0500

    next config updated

--- next.config.js
+++ next.config.js
@@ -4,27 +4,10 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cheerup2.theme-sphere.com",
-      },
-      {
-        protocol: "https",
         hostname: "api.sitebuilderz.com",
       },
-      {
-        protocol: "https",
-        hostname: "www.custom-wheels-car-rims.com",
-      },
-      {
-        protocol: "https",
-        hostname: "custom-wheels-car-rims.com",
-      },
     ],
-    domains: [
-      "cheerup2.theme-sphere.com",
-      "api.sitebuilderz.com",
-      "www.custom-wheels-car-rims.com",
-      "custom-wheels-car-rims.com",
-    ],
+    domains: ["api.sitebuilderz.com"],
   },
   reactStrictMode: true,
 };
