f7eea6fb49b09e153904ebcbf7a7a30defd1ffb1
Usama Bhatti <usamamustafa393@gmail.com>
2024-11-18 09:45:04 -0800

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -402,7 +402,7 @@ export async function getServerSideProps({ req }) {
       footer_type: footer_type?.data[0]?.value || {},
       tag_list: tag_list?.data[0]?.value || null,
       all_data,
-      page,
+      // page,
     },
   };
 }
